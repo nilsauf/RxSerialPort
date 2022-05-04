21205bdaa68c02d74ff31d08971f43dd4e5db542
Nils Aufschläger <mail@nilsauf.de>
2022-05-04 23:19:45 +0200

    Remove automatic opening and closing of serialport in connect

--- RxSerialPort/RxSerialPort.cs
+++ RxSerialPort/RxSerialPort.cs
@@ -7,33 +7,21 @@
 
 	public static class RxSerialPort
 	{
-		public static IObservable<SerialPortEvent> Connect(this SerialPort serialPort, bool autoOpen = true)
+		public static IObservable<SerialPortEvent> Connect(
+			this SerialPort serialPort)
 		{
-			return Observable.Return(serialPort)
-				.Do(serialPort =>
-				{
-					if (serialPort.IsOpen == false && autoOpen == true)
-					{
-						serialPort.Open();
-					}
-				})
-				.SelectMany(serialPort =>
-				{
-					var serialPortEvents = serialPort.Events();
+			var serialPortEvents = serialPort.Events();
 
-					return serialPortEvents.DataReceived
-							.Select(line => new SerialPortEvent(serialPort, serialPort.ReadExisting()))
-						.Merge(serialPortEvents.Disposed
-							.Select(_ => new SerialPortEvent(serialPort)))
-						.Merge(serialPortEvents.ErrorReceived
-							.Select(errorEventArgs => new SerialPortEvent(serialPort, errorEventArgs.EventType)))
-						.Merge(serialPortEvents.PinChanged
-							.Select(pinChangedEventArgs => new SerialPortEvent(serialPort, pinChangedEventArgs.EventType)));
-				})
-				.TakeUntil(@event => @event.EventType == SerialPortEventType.Disposed)
-				.Publish()
-				.AutoConnect()
-				.AsObservable();
+			return serialPortEvents.DataReceived
+				.Select(line => new SerialPortEvent(serialPort, serialPort.ReadExisting()))
+			.Merge(serialPortEvents.Disposed
+				.Select(_ => new SerialPortEvent(serialPort)))
+			.Merge(serialPortEvents.ErrorReceived
+				.Select(errorEventArgs => new SerialPortEvent(serialPort, errorEventArgs.EventType)))
+			.Merge(serialPortEvents.PinChanged
+				.Select(pinChangedEventArgs => new SerialPortEvent(serialPort, pinChangedEventArgs.EventType)))
+			.TakeUntil(@event => @event.EventType == SerialPortEventType.Disposed)
+			.AsObservable();
 		}
 	}
 }
