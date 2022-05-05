75bd064fc523d72a6cce4c58f05bde4e9692089f
Nils Aufschläger <mail@nilsauf.de>
2022-05-05 22:06:31 +0200

    add extensions for observables of serial port events

---
+++ RxSerialPort/RxSerialPort_ObservableExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace System.IO.Ports
+{
+	using System;
+	using System.Linq;
+	using System.Reactive;
+	using System.Reactive.Linq;
+
+	public static partial class RxSerialPort
+	{
+		public static IObservable<string> WatchData(this IObservable<SerialPortEvent> portEvents)
+		{
+			return portEvents.Where(@event => @event.EventType == SerialPortEventType.DataReceived)
+				.Where(@event => string.IsNullOrEmpty(@event.Data) == false)
+				.Select(@event => @event.Data);
+		}
+
+		public static IObservable<SerialError> WatchErrors(this IObservable<SerialPortEvent> portEvents)
+		{
+			return portEvents.Where(@event => @event.EventType == SerialPortEventType.ErrorReceived)
+				.Where(@event => @event.ErrorType.HasValue)
+				.Select(@event => @event.ErrorType.Value);
+		}
+
+		public static IObservable<SerialPinChange> WatchPinChanges(this IObservable<SerialPortEvent> portEvents)
+		{
+			return portEvents.Where(@event => @event.EventType == SerialPortEventType.PinChanged)
+				.Where(@event => @event.PinChangeType.HasValue)
+				.Select(@event => @event.PinChangeType.Value);
+		}
+
+		public static IObservable<Unit> WatchDisposing(this IObservable<SerialPortEvent> portEvents)
+		{
+			return portEvents.Where(@event => @event.EventType == SerialPortEventType.Disposed)
+				.Select(_ => Unit.Default);
+		}
+	}
+}
