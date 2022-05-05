99080b9de48a36453055c50c960de2cd13be51c7
Nils Aufschläger <mail@nilsauf.de>
2022-05-06 00:48:48 +0200

    Add extensions for  IObservable<string>

---
+++ RxSerialPort/RxSerialPort.Extensions.WriteLineTo.cs
@@ -0,0 +1,75 @@
+﻿namespace System.IO.Ports
+{
+	using System;
+
+	public static partial class RxSerialPort
+	{
+		public static IDisposable WriteLineTo(
+			this IObservable<string> source,
+			string portName,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (string.IsNullOrWhiteSpace(portName))
+			{
+				throw new ArgumentException($"'{nameof(portName)}' cannot be null or whitespace.", nameof(portName));
+			}
+
+			return source.WriteLineTo(() => new SerialPort(portName), errorAction, completedAction);
+		}
+
+		public static IDisposable WriteLineTo(
+			this IObservable<string> source,
+			Func<SerialPort> portFactoy,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (portFactoy is null)
+			{
+				throw new ArgumentNullException(nameof(portFactoy));
+			}
+
+			var serialPort = portFactoy();
+			return source.WriteLineTo(serialPort,
+				ex =>
+				{
+					serialPort.Dispose();
+					errorAction?.Invoke(ex);
+				},
+				() =>
+				{
+					serialPort.Dispose();
+					completedAction?.Invoke();
+				});
+		}
+
+		public static IDisposable WriteLineTo(
+			this IObservable<string> source,
+			SerialPort serialPort,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (serialPort is null)
+			{
+				throw new ArgumentNullException(nameof(serialPort));
+			}
+
+			return source.Subscribe(CreateWriteLineObserver(() => serialPort, errorAction, completedAction));
+		}
+	}
+}
---
+++ RxSerialPort/RxSerialPort.Extensions.WriteTo.cs
@@ -0,0 +1,75 @@
+﻿namespace System.IO.Ports
+{
+	using System;
+
+	public static partial class RxSerialPort
+	{
+		public static IDisposable WriteTo(
+			this IObservable<string> source,
+			string portName,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (string.IsNullOrWhiteSpace(portName))
+			{
+				throw new ArgumentException($"'{nameof(portName)}' cannot be null or whitespace.", nameof(portName));
+			}
+
+			return source.WriteTo(() => new SerialPort(portName), errorAction, completedAction);
+		}
+
+		public static IDisposable WriteTo(
+			this IObservable<string> source,
+			Func<SerialPort> portFactoy,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (portFactoy is null)
+			{
+				throw new ArgumentNullException(nameof(portFactoy));
+			}
+
+			var serialPort = portFactoy();
+			return source.WriteTo(serialPort,
+				ex =>
+				{
+					serialPort.Dispose();
+					errorAction?.Invoke(ex);
+				},
+				() =>
+				{
+					serialPort.Dispose();
+					completedAction?.Invoke();
+				});
+		}
+
+		public static IDisposable WriteTo(
+			this IObservable<string> source,
+			SerialPort serialPort,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (serialPort is null)
+			{
+				throw new ArgumentNullException(nameof(serialPort));
+			}
+
+			return source.Subscribe(CreateWriteObserver(() => serialPort, errorAction, completedAction));
+		}
+	}
+}
