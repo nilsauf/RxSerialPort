292f9a470d875c26714ef77e233b6a63abbd2d3a
Nils Aufschläger <mail@nilsauf.de>
2022-05-06 13:59:44 +0200

    Move serialport creation into factories of extensions

--- RxSerialPort/RxSerialPort.Extensions.WriteTo.cs
+++ RxSerialPort/RxSerialPort.Extensions.WriteTo.cs
@@ -25,6 +25,92 @@
 
 		public static IDisposable WriteTo(
 			this IObservable<string> source,
+			string portName,
+			int baudRate,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (string.IsNullOrWhiteSpace(portName))
+			{
+				throw new ArgumentException($"'{nameof(portName)}' cannot be null or whitespace.", nameof(portName));
+			}
+
+			return source.WriteTo(() => new SerialPort(portName, baudRate), errorAction, completedAction);
+		}
+
+		public static IDisposable WriteTo(
+			this IObservable<string> source,
+			string portName,
+			int baudRate,
+			Parity parity,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (string.IsNullOrWhiteSpace(portName))
+			{
+				throw new ArgumentException($"'{nameof(portName)}' cannot be null or whitespace.", nameof(portName));
+			}
+
+			return source.WriteTo(() => new SerialPort(portName, baudRate, parity), errorAction, completedAction);
+		}
+
+		public static IDisposable WriteTo(
+			this IObservable<string> source,
+			string portName,
+			int baudRate,
+			Parity parity,
+			int dataBits,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (string.IsNullOrWhiteSpace(portName))
+			{
+				throw new ArgumentException($"'{nameof(portName)}' cannot be null or whitespace.", nameof(portName));
+			}
+
+			return source.WriteTo(() => new SerialPort(portName, baudRate, parity, dataBits), errorAction, completedAction);
+		}
+
+		public static IDisposable WriteTo(
+			this IObservable<string> source,
+			string portName,
+			int baudRate,
+			Parity parity,
+			int dataBits,
+			StopBits stopBits,
+			Action<Exception> errorAction = null,
+			Action completedAction = null)
+		{
+			if (source is null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (string.IsNullOrWhiteSpace(portName))
+			{
+				throw new ArgumentException($"'{nameof(portName)}' cannot be null or whitespace.", nameof(portName));
+			}
+
+			return source.WriteTo(() => new SerialPort(portName, baudRate, parity, dataBits, stopBits), errorAction, completedAction);
+		}
+
+		public static IDisposable WriteTo(
+			this IObservable<string> source,
 			Func<SerialPort> portFactoy,
 			Action<Exception> errorAction = null,
 			Action completedAction = null)
