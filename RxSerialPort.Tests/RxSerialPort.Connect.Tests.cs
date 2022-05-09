994118c3821031e8bb8178fd4219c2912acfdb34
Nils Aufschläger <mail@nilsauf.de>
2022-05-09 22:07:32 +0200

    add first tests for connect(andread) + creating observers

--- RxSerialPort.Tests/RxSerialPort.Connect.Tests.cs
+++ RxSerialPort.Tests/RxSerialPort.Connect.Tests.cs
@@ -1,47 +1,161 @@
 ﻿namespace System.IO.Ports.Tests
 {
+	using System;
 	using System.Reactive.Linq;
 	using System.Threading.Tasks;
 	using Xunit;
 
 	public class RxSerialPort_Connect_Tests
 	{
-		[Fact()]
-		public async Task Connect_Test()
+		[Fact]
+		public void ConnectAndRead_Ex_Creation_Normal()
 		{
-			var serialPort = new SerialPort(SerialPort.GetPortNames()[0]);
-			var serialPort2 = new SerialPort(SerialPort.GetPortNames()[1]);
+			var serialPort = new SerialPort();
+			Func<SerialPort, string> readFunction = port => string.Empty;
 
-			serialPort.Connect(port => port.ReadExisting())
-				.WatchData()
-				.Subscribe(data => { }, ex => { }, () => { });
+			var serialPortObservable = serialPort.Connect(readFunction);
+
+			Assert.NotNull(serialPortObservable);
+		}
+
+		[Fact]
+		public void ConnectAndRead_Ex_Creation_PortNull()
+		{
+			SerialPort serialPort = null;
+			Func<SerialPort, string> readFunction = port => string.Empty;
+
+			Assert.Throws<ArgumentNullException>(() => serialPort.Connect(readFunction));
+		}
+
+		[Fact]
+		public void ConnectAndRead_Creation_Normal()
+		{
+			Func<SerialPort, string> readFunction = port => string.Empty;
+			Func<SerialPort> serialPortFactory = () => new SerialPort();
+
+			var serialPortObservable = RxSerialPort.Connect(serialPortFactory, readFunction);
+
+			Assert.NotNull(serialPortObservable);
+		}
+
+		[Fact]
+		public void ConnectAndRead_Creation_PortFactoryNull()
+		{
+			Func<SerialPort, string> readFunction = port => string.Empty;
+			Func<SerialPort> serialPortFactory = null;
+
+			Assert.Throws<ArgumentNullException>(() => RxSerialPort.Connect(serialPortFactory, readFunction));
+		}
+
+		[Fact]
+		public void ConnectAndRead_Creation_PortFactoryReturnsNull()
+		{
+			Func<SerialPort, string> readFunction = port => string.Empty;
+			Func<SerialPort> serialPortFactory = () => null;
+			bool errorCalled = false;
+
+			var sub = RxSerialPort.Connect(serialPortFactory, readFunction)
+				.Subscribe(
+					@event => Assert.True(false),
+					ex =>
+					{
+						errorCalled = true;
+						Assert.IsType<InvalidOperationException>(ex);
+					},
+					() => Assert.True(false));
+
+			sub.Dispose();
+
+			Assert.True(errorCalled);
+		}
+
+		[Fact]
+		public void Connect_Ex_Creation_Normal()
+		{
+			var serialPort = new SerialPort();
+
+			var serialPortObservable = serialPort.Connect();
+
+			Assert.NotNull(serialPortObservable);
+		}
+
+		[Fact]
+		public void Connect_Ex_Creation_PortNull()
+		{
+			SerialPort serialPort = null;
+
+			Assert.Throws<ArgumentNullException>(() => serialPort.Connect());
+		}
+
+		[Fact]
+		public void Connect_Creation_Normal()
+		{
+			Func<SerialPort> serialPortFactory = () => new SerialPort();
+
+			var serialPortObservable = RxSerialPort.Connect(serialPortFactory);
+
+			Assert.NotNull(serialPortObservable);
+		}
+
+		[Fact]
+		public void Connect_Creation_PortFactoryNull()
+		{
+			Func<SerialPort> serialPortFactory = null;
+
+			Assert.Throws<ArgumentNullException>(() => RxSerialPort.Connect(serialPortFactory));
+		}
+
+#if TEST_WITH_REAL_PORTS
+		[Fact()]
+		public async Task ConnectAndRead_Ex_ReceivingData_Normal()
+		{
+			string testLine = "Hello Port";
+			bool receivedCalled = false;
+			using var serialPort = new SerialPort(SerialPort.GetPortNames()[0]);
+			using var serialPort2 = new SerialPort(SerialPort.GetPortNames()[1]);
 
 			serialPort.Open();
 			serialPort2.Open();
+
+			using var sub = serialPort.Connect(port => port.ReadExisting().Trim('\n'))
+				.WatchData()
+				.Subscribe(data =>
+				{
+					Assert.Equal(testLine, data);
+					receivedCalled = true;
+				});
+
+			serialPort2.WriteLine(testLine);
+
+			await Task.Delay(500);
+
+			Assert.True(receivedCalled);
+		}
+
+		[Fact()]
+		public async Task ConnectAndRead_ReceivingData_Normal()
+		{
+			string testLine = "Hello Port";
+			bool receivedCalled = false;
+			using var serialPort2 = new SerialPort(SerialPort.GetPortNames()[1]);
+			serialPort2.Open();
+
+			using var sub = RxSerialPort.Connect(
+					() => new SerialPort(SerialPort.GetPortNames()[0]),
+					port => port.ReadExisting().Trim('\n'))
+				.WatchData()
+				.Subscribe(data =>
+				{
+					Assert.Equal(testLine, data);
+					receivedCalled = true;
+				});
+
 			serialPort2.WriteLine("Hello Port");
 
 			await Task.Delay(500);
 
-			serialPort2.Dispose();
-			serialPort.Dispose();
+			Assert.True(receivedCalled);
 		}
-
-		[Fact()]
-		public async Task ConnectByName()
-		{
-			var serialPort2 = new SerialPort(SerialPort.GetPortNames()[1]);
-
-			var sub = RxSerialPort.Connect(SerialPort.GetPortNames()[0], port => port.ReadExisting())
-				.Select(@event => @event.Data)
-				.Subscribe(data => { }, ex => { }, () => { });
-
-			serialPort2.Open();
-			serialPort2.WriteLine("Hello Port");
-
-			await Task.Delay(1000);
-
-			serialPort2.Dispose();
-			sub.Dispose();
-		}
+#endif
 	}
 }
---
+++ RxSerialPort.Tests/RxSerialPort.Observer.Tests.cs
@@ -0,0 +1,98 @@
+﻿namespace System.IO.Ports.Tests
+{
+	using System;
+	using Xunit;
+
+	public class RxSerialPort_Observer_Tests
+	{
+		[Fact]
+		public void CreateObserver_Creation_Normal()
+		{
+			Action<SerialPort, string> writeAction = (port, line) => { };
+
+			var serialPortObserver = RxSerialPort.CreateObserver(() => new SerialPort(), writeAction);
+
+			Assert.NotNull(serialPortObserver);
+		}
+
+		[Fact]
+		public void CreatObserver_Creation_WriteActionNull()
+		{
+			Action<SerialPort, string> writeAction = null;
+
+			Assert.Throws<ArgumentNullException>(() => RxSerialPort.CreateObserver(() => new SerialPort(), writeAction));
+		}
+
+		[Fact]
+		public void CreatObserver_Creation_FactoryNull()
+		{
+			Action<SerialPort, string> writeAction = (port, line) => { };
+			Func<SerialPort> portFactory = null;
+
+			Assert.Throws<ArgumentNullException>(() => RxSerialPort.CreateObserver(portFactory, writeAction));
+		}
+
+		[Fact]
+		public void CreatObserver_Creation_FactoryCreatesNull()
+		{
+			Action<SerialPort, string> writeAction = (port, line) => { };
+			Func<SerialPort> portFactory = () => null;
+
+			Assert.Throws<InvalidOperationException>(() => RxSerialPort.CreateObserver(portFactory, writeAction));
+		}
+
+		[Fact]
+		public void AsObserver_Creation_Normal()
+		{
+			var serialPort = new SerialPort();
+			Action<SerialPort, string> writeAction = (port, line) => { };
+
+			var serialPortObserver = serialPort.AsObserver(writeAction);
+
+			Assert.NotNull(serialPortObserver);
+		}
+
+		[Fact]
+		public void AsObserver_Creation_WriteActionNull()
+		{
+			var serialPort = new SerialPort();
+			Action<SerialPort, string> writeAction = null;
+
+			Assert.Throws<ArgumentNullException>(() => serialPort.AsObserver(writeAction));
+		}
+
+		[Fact]
+		public void AsObserver_Creation_SerialPortNull()
+		{
+			SerialPort serialPort = null;
+			Action<SerialPort, string> writeAction = (port, line) => { };
+
+			Assert.Throws<ArgumentNullException>(() => serialPort.AsObserver(writeAction));
+		}
+
+#if TEST_WITH_REAL_PORTS
+		[Fact]
+		public void AsObserver_Function_OnNext_Open()
+		{
+			string testLine = "Hello World";
+			bool writeActionCalled = false;
+			using var serialPort = new SerialPort(SerialPort.GetPortNames()[0]);
+			serialPort.Open();
+			var writeAction = new Action<SerialPort, string>((port, line) =>
+			{
+				Assert.NotNull(port);
+				Assert.Same(serialPort, port);
+				Assert.Equal(line, testLine);
+				writeActionCalled = true;
+			});
+
+			var serialPortObserver = serialPort.AsObserver(writeAction);
+
+			serialPortObserver.OnNext(testLine);
+
+			Assert.True(writeActionCalled);
+		}
+
+#endif
+	}
+}
