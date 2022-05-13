19fb59d8bb3c31debff12213af84379ac5c16d89
Nils Aufschläger <mail@nilsauf.de>
2022-05-13 20:53:54 +0200

    add testtools

---
+++ RxSerialPort.Tests/RxSerialPort.TestTools.cs
@@ -0,0 +1,20 @@
+﻿namespace System.IO.Ports.Tests
+{
+	public static class RxSerialPort_TestTools
+	{
+		public static void OpenSafelyForTest(this SerialPort serialPort)
+		{
+			while (serialPort.IsOpen == false)
+			{
+				try
+				{
+					serialPort.Open();
+				}
+				catch (UnauthorizedAccessException)
+				{
+
+				}
+			}
+		}
+	}
+}
